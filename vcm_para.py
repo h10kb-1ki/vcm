4c8e06cb9c72e6bf3b78ffd39056a6694d5f1f68
hmill <hmillionsalt@gmail.com>
2021-07-11 14:46:15 +0900

    add new file

--- vcm_para.py
+++ vcm_para.py
@@ -20,7 +20,8 @@ weight = st.sidebar.slider('■体重（kg）', 20, 150, 50)
 SCr = st.sidebar.slider('■SCr', 0.00, 2.50, 0.60)
 
 st.sidebar.title('設定値')
-dose = st.sidebar.selectbox('■投与量（mg）', list((250, 500, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000)), index=5)
+#dose = st.sidebar.selectbox('■投与量（mg）', list((250, 500, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000)), index=5)
+dose = st.sidebar.number_input('■投与量（mg）', 1)
 tau = st.sidebar.selectbox('■投与間隔（hr）', list((6, 8, 12, 24, 48)), index=3)
 
 
@@ -83,11 +84,13 @@ t_half = math.log(2)/kel
 't1/2 (hr) = ', round(t_half, 1)
 Ctrough = ((dose/Vd)/(1-math.exp(-kel*tau)))*math.exp(-kel*tau)
 'Ctrough (mg/L) = ', round(Ctrough, 2)
+AUC = dose/CLvcm
+'AUC (mg・hr/L) = ', round(AUC, 1)
 
 
 st.title('VCMパラメータ Fitting')
 f_Vd = st.slider('Vd', 0.0, 150.0, Vd)
-f_kel = st.slider('kel（x 10^-3）', 0, 1000, int(kel*1000))
+f_kel = st.slider('kel（x 0.001）', 0, 1000, int(kel*1000))
 #f_Vd = st.number_input('Vd', 1.0, 150.0, Vd)
 #f_kel = st.number_input('kel  *小数点第3位以下の数字も計算には反映されます', 0.000, 2.000, kel)
 
@@ -98,3 +101,5 @@ f_t_half = math.log(2) / (f_kel/1000)
 't1/2 (/hr) = ', round(f_t_half, 1)
 f_Ctrough = ((dose/f_Vd)/(1-math.exp(-(f_kel/1000)*tau)))*math.exp(-(f_kel/1000)*tau)
 'Ctrough (mg/L) = ', round(f_Ctrough, 2)
+f_AUC = dose/f_CLvcm
+'AUC (mg・hr/L) = ', round(f_AUC, 1)
